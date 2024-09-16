7fc73e66af1c9a8da8c80e31486bbbf1bdbe35aa
Akila Ishan Meegoda <it22640116@my.sliit.lk>
2024-09-16 14:09:08 +0530

    dropdown

--- app/(adminTabs)/addDoctor.jsx
+++ app/(adminTabs)/addDoctor.jsx
@@ -1,12 +1,49 @@
-import { View, Text, Image, TextInput, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import RNPickerSelect from "react-native-picker-select";
+import SelectBox from "react-native-multi-selectbox";
+import { xorBy } from "lodash";
 
 import AddDoctorHeader from "../../components/Admin/AddDoctorHeader";
 
-const addDoctor = () => {
+const K_OPTIONS = [
+  { item: "Juventus", id: "JUVE" },
+  { item: "Real Madrid", id: "RM" },
+  { item: "Barcelona", id: "BR" },
+  { item: "PSG", id: "PSG" },
+  { item: "FC Bayern Munich", id: "FBM" },
+  { item: "Manchester United FC", id: "MUN" },
+  { item: "Manchester City FC", id: "MCI" },
+  { item: "Everton FC", id: "EVE" },
+  { item: "Tottenham Hotspur FC", id: "TOT" },
+  { item: "Chelsea FC", id: "CHE" },
+  { item: "Liverpool FC", id: "LIV" },
+  { item: "Arsenal FC", id: "ARS" },
+  { item: "Leicester City FC", id: "LEI" },
+];
+
+const AddDoctor = () => {
+  const [selectedTeam, setSelectedTeam] = useState({});
+  const [selectedTeams, setSelectedTeams] = useState([]);
+
+  // Move these functions inside the component
+  const onChange = (val) => {
+    setSelectedTeam(val);
+  };
+
+  const onMultiChange = (item) => {
+    setSelectedTeams(xorBy(selectedTeams, [item], "id"));
+  };
+
   return (
-    <View>
+    <ScrollView>
       <AddDoctorHeader />
       <View className="p-6">
         <Text className="font-[poppins-bold] text-lg">Add new doctor</Text>
@@ -29,19 +66,16 @@ const addDoctor = () => {
             placeholder="Enter full name"
             className="p-2 px-4 mt-2 text-lg bg-white border-2 border-gray-200 rounded-md "
           />
-           <TextInput
+          <TextInput
             placeholder="Enter years of experience"
             keyboardType="numeric"
             className="p-2 px-4 mt-2 text-lg bg-white border-2 border-gray-200 rounded-md "
           />
-          <View className="mt-2 text-black bg-white border-2 border-gray-200 rounded-md text-md ">
+          <View className="mt-2 text-black bg-white border-2 border-gray-200 rounded-md text-md">
             <RNPickerSelect
               className="font-[poppins-bold]"
               onValueChange={(value) => console.log(value)}
-              placeholder={{
-                label: "Select Specialization",
-                value: null,
-              }}
+              placeholder={{ label: "Select Specialization", value: null }}
               items={[
                 { label: "Cardiologist", value: "Cardiologist" },
                 { label: "Dermatologist", value: "Dermatologist" },
@@ -50,23 +84,21 @@ const addDoctor = () => {
                 { label: "Oncologist", value: "Oncologist" },
                 { label: "Pulmonologist", value: "Pulmonologist" },
                 { label: "Nephrologist", value: "Nephrologist" },
-                { label: "Otolaryngologist ", value: "Otolaryngologist" },
+                { label: "Otolaryngologist", value: "Otolaryngologist" },
               ]}
             />
           </View>
-          <View className="mt-2 text-black bg-white border-2 border-gray-200 rounded-md text-md " style={styles.container}>
+          <View className="mt-2 text-black bg-white border-2 border-gray-200 rounded-md text-md">
             <RNPickerSelect
               className="font-[poppins-bold]"
-              style={styles.sourcePicker}
               onValueChange={(value) => console.log(value)}
-              placeholder={{
-                label: "Select Hospital",
-                value: null,
-              }}
-              itemStyle={{ backgroundColor: "grey", color: "blue", fontFamily:"poppins", fontSize:17 }}
+              placeholder={{ label: "Select Hospital", value: null }}
               items={[
                 { label: "Nawaloka Hospital", value: "Nawaloka Hospital" },
-                { label: "Asiri Central Hospital", value: "Asiri Central Hospital" },
+                {
+                  label: "Asiri Central Hospital",
+                  value: "Asiri Central Hospital",
+                },
                 { label: "Hemas Hospital", value: "Hemas Hospital" },
                 { label: "Ninewells Hospital", value: "Ninewells Hospital" },
                 { label: "Durdans Hospital", value: "Durdans Hospital" },
@@ -80,18 +112,24 @@ const addDoctor = () => {
             numberOfLines={3}
             className="p-2 px-4 mt-2 text-lg bg-white border-2 border-gray-200 rounded-md "
           />
-         
-        </View>
+
+            <View className="mx-4 mt-4 ">
+            <Text style={{ fontSize: 20, paddingBottom: 10 }}>
+              Select times
+            </Text>
+            <SelectBox
+              label="Select multiple"
+              options={K_OPTIONS}
+              selectedValues={selectedTeams}
+              onMultiSelect={onMultiChange} // Corrected here
+              onTapClose={onMultiChange} // Corrected here
+              isMulti
+            />
+            </View>
+          </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
-export default addDoctor;
-
-const styles = StyleSheet.create({
-  container : {
-      fontFamily:"poppins-bold",
-      fontSize:"10px"
-  },
-});
+export default AddDoctor;
