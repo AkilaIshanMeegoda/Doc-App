79874780e8d6f44250653821a73f716740901c6c
chala16 <chalanakaveeshami@gmail.com>
2024-09-17 19:35:35 +0530

    add doctor profile component

--- app/hospitalprofile/[hospital].jsx
+++ app/hospitalprofile/[hospital].jsx
@@ -19,6 +19,8 @@ const hospital = () => {
         <HospitalProfile />
         <ScrollView horizontal={true} contentContainerStyle={{ flexDirection: 'row' }}>
          <DoctorProfile />
+         <DoctorProfile />
+         <DoctorProfile />
           {/* Add more DoctorProfile components as needed */}
         </ScrollView>
         <OptionComponent />
